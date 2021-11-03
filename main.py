ae630c42f82660dfaa12d96b31fa24aa8d953e3d
Alex <48862843+LobanAB@users.noreply.github.com>
2021-11-03 19:30:32 +0300

    add Telegram bot

--- main.py
+++ main.py
@@ -8,54 +8,52 @@ import telegram
 
 
 def get_last_timestamp(dvmn_api_token):
-    dvmn_api_url= 'https://dvmn.org/api/user_reviews/'
-    headers = {
-        'Authorization': dvmn_api_token
-    }
+    dvmn_api_url = 'https://dvmn.org/api/user_reviews/'
+    headers = {'Authorization': dvmn_api_token}
     response = requests.get(dvmn_api_url, headers=headers)
     response.raise_for_status()
     response_json = response.json()
     return response_json['results'][0]['timestamp']
 
 
-def get_dvmn_api(dvmn_api_token, timestamp):
-    # dvmn_api_url= 'https://dvmn.org/api/user_reviews/'
+def get_dvmn_api_json(dvmn_api_token, timestamp):
     dvmn_api_url_long_polling = 'https://dvmn.org/api/long_polling/'
-    headers = {
-        'Authorization': dvmn_api_token
-    }
-    payload = {'timestamp': timestamp
-               }
-    response = requests.get(dvmn_api_url_long_polling, headers=headers, params=payload, timeout=5)
+    headers = {'Authorization': dvmn_api_token}
+    payload = {'timestamp': timestamp}
+    response = requests.get(dvmn_api_url_long_polling, headers=headers, params=payload)
     response.raise_for_status()
-    response_json = response.json()
-    if response_json['status'] == 'found':
-        print(response_json)
-        return response_json['last_attempt_timestamp']
-    else:
-        print('timeout')
-        return response_json['timestamp_to_request']
+    return response.json()
 
 
 def main():
     load_dotenv()
     dvmn_api_token = os.getenv('DVMN_API_TOKEN')
-    timestamp = get_last_timestamp(dvmn_api_token)
     bot = telegram.Bot(token=os.getenv('TG_API_TOKEN'))
-    print(bot.get_me())
-    updates = bot.get_updates()
-    print(updates[0])
-    bot.send_message(text='Привет', chat_id=465065578)
-    '''
+    timestamp = get_last_timestamp(dvmn_api_token)
     while True:
         try:
-            timestamp = get_dvmn_api(dvmn_api_token, timestamp)
+            response_json = get_dvmn_api_json(dvmn_api_token, timestamp)
+            if response_json['status'] == 'found':
+                timestamp = response_json['last_attempt_timestamp']
+                lesson_title = response_json['new_attempts'][0]['lesson_title']
+                lesson_url = response_json['new_attempts'][0]['lesson_url']
+                if response_json['new_attempts'][0]['is_negative']:
+                    text = f'Ваша работа "{lesson_title}" проверена. К сожалению, в работе есть ошибки! ' \
+                           f'Вот ссылка: {lesson_url}'
+                else:
+                    text = f'Ваша работа "{lesson_title}" проверена. ' \
+                           f'Все хорошо, можно приступать к следующему уроку! ' \
+                           f'Вот ссылка: {lesson_url}'
+                bot.send_message(text=text, chat_id=465065578)
+            else:
+                print('timeout')
+                timestamp = response_json['timestamp_to_request']
         except requests.exceptions.Timeout:
             print('timeout')
         except requests.exceptions.ConnectionError:
             print('ConnectionError')
             time.sleep(5)
-    '''
+
 
 if __name__ == '__main__':
     main()
